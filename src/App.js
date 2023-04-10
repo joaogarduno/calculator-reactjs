035f966593a8bbdd0b21495ad181e7ed8d36fa63
Joao Garduño <joao.garduno@gmail.com>
2023-04-10 13:26:39 -0600

    Tema 2: Contenedor Principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+// Para poder importar una imagen necesitamos una sentencia de importacion
+import logoArtheFacto from './img/arthefacto-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div className='App'>
+
+      {/* LOGO */}
+      <div className='logo-contenedor'>
+        {/* Las llaves nos permiten insertar codigo de JavaScript dentro de nuestro JSX, nuestra descripcion de la estructura del componente */}
+        {/* Entonces... lo que colocamos entre las llaves de "src={logoArtheFacto}" nos referimos a una variable que creamos en nuestro archivo */}
+        <img
+          src={logoArtheFacto}
+          className='log-at'
+          alt='Logo de  ArtheFacto'
+        />
+      </div>
+
+      {/* CONTENEDOR CALCULADORA */}
+      <div className='contenedor-calculadora'>
+        {/* Dentro de este <div> vamos a tener todos los botones las filas y los distintos elementos de la calculadora como su pantalla */}
+        
+      </div>
     </div>
   );
 }
