66340e47e5d1778e1941424729b96ea3cd4af8ff
Joao Garduño <joao.garduno@gmail.com>
2023-04-13 19:15:55 -0600

    Tema 12: Mostrar input en pantalla

--- src/App.js
+++ src/App.js
@@ -4,9 +4,32 @@ import logoArtheFacto from './img/arthefacto-logo.png';
 import Boton from './component/boton';
 import Pantalla from './component/input';
 import BotonClear from './component/btnClear';
-
+// Para poder mostrar el valor del botón en la pantalla necesitamos hacer uso de los "Hooks" y es "Hook" que ocuparemos es el de "estado" es decir "userState"
+// Recuerda que para importar un Hook necesitamos de las llaves.
+// Podemos agregar este "hook" al final o sobre los componentes, es decir hasta arriba de este archivo
+import { useState } from 'react';
+// Ya tenemos nuetsro "hook" eso significa que ya podemos asignarle un estado a nuestro componente, en este caso es nuestro componente principal de la aplicacion.
+// Trabajaremos con el estado directamente.
+// NOTA: Otro enfoque para poder realizarlo es creando otro componente llamado "calculadora" y que ese componente tuviera ese estado, cualquiera de los dos es valido.
 
 function App() {
+  // Para crear ese estado ¿qué es lo que ha ingresado el usuario hasta el momento?
+  const [input, setInput] = useState('');
+  // En esta constante estamos creando un estado para la aplicacion llamado "input" este "input" que significa "valor ingresado por el usuario" se mantendra como el estado del componente "input" y por supuesto que tenemo una funcion para poder actualizar el "app o input"
+  // Inicialmente ese "input" sera una cadena vacia, de esta manera no veremos nada en la pantalla 
+
+  // Lo primero que haremos es asignar ese "input" como el valor de la pantalla para que cuando se actualice, actualice su valor. Vamos a nuestro componente "Pantalla" y vamos a pasar el "prop" "input" con el valor del estado "input"
+
+  // Recorda que inicialmente vamos a empezar con una cadena vacia y luego vamos a definir otra funcion.
+  // Tenemos la funcion "agregar input"
+  const agregarInput = val => {
+    // ¿Qué es lo que hace esta funcion? esta funcion va actualizar el input.
+    // Y dentro de los parentesis le vamos a pasar el nuevo valor de "input", y cual va a ser? el input que teniamos anteriormente, concatenado con el nuevo valor.
+    setInput(input + val); 
+  };
+
+
+
   return (
     <div className='App'>
 
@@ -24,7 +47,8 @@ function App() {
       {/* CONTENEDOR CALCULADORA */}
       <div className='contenedor-calculadora'>
         {/* Ahora que tenemos nuestro componente Pantalla podemos mostrarlo en nuestra estructura principal */}
-        <Pantalla />
+        <Pantalla input={input} />
+        {/* En este caso el "prop" y la variable como tal el estado tienen el mismo nombre no necesariamente va a ocurrir y eso no afectara en nada el funcionamiento del componente */}
 
         {/* Dentro de este <div> vamos a tener todos los botones las filas y los distintos elementos de la calculadora como su pantalla */}
 
