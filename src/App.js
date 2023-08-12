65fc30b6c8a1af8d92d282a2c764d180117329b7
Joao Garduño <joao.garduno@gmail.com>
2023-08-12 14:58:59 -0600

    Tema 1: Comenzar el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+// Para poder importar la imagen necesitamos una sentencia de importacion
+// con la palabra "arthefacto" nos estamos refieriendo a una variable que creamos en nuestro archivo
+import arthefactologo from './img/arthefacto_favicon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      
+      {/* <<<--- 01 - COMENZAR EL PROYECTO --->>> */}
+      {/* Para esta aplicación vamos a crear varios componentes, iniciaremos con la estructura de nuestra aplicación */}
+
+      {/* Tenemos un logo de ArtheFacto en la parte superior */}
+      <div className='arthefacto-logo-contenedor'>
+        {/* Recordar que las lleves {} nos permiten insertar código de JavaScript dentro de nuestro JSX, de esta forma podemos agregar nuestra descripcion de la estructura del componente y ese nombre lo podemos utilizar como una variable */}
+        <img 
+          src={arthefactologo}
+          className='arthefacto-logo'
+          alt='Logo de ArtheFacto Inc.'
+        /> 
+      </div>
+
     </div>
   );
 }
